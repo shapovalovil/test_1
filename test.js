01e1f709a1e093210009d95bdb17323986a17de9
Iosif <shapovalov.iosif@yandex.ru>
2020-01-01 12:51:24 +0300

    index.ht,l

---
+++ test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let money = 0;
+let time = new Date;
+money = prompt("Ваш бюджет на месяц?",100000);
+console.log(money);
+time = prompt("Введите дату в формате YYYY-MM-DD","2020.01.01");
+console.log(time);
+console.log(typeof(time));
+
+let appData = {
+budjet: money,
+timeData: time,
+expenses: {},
+optionalExpense: {},
+income: [],
+savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+
+    alert(appData.budjet / 30);
