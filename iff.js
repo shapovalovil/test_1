3d8f70063c0c685ee295dc31c10a5fb5871c1441
Iosif <shapovalov.iosif@yandex.ru>
2020-01-01 18:06:15 +0300

    bigg

---
+++ iff.js
@@ -0,0 +1,21 @@
+let message = 'директор';
+let login='диdsректор';
+
+(login=='сотрудник') ? message = 'привет': 
+(login=='директор') ? message = 'здравствуйте':
+(login=='') ? message = 'нет логина':message = 'gji`k djy';
+
+console.log(message);
+let t = 20;
+//(t>10) ? console.log("больше") : console.log("меньше");
+var abc = "abcdefghijklmnopqrstuvwxyz";
+
+do{
+   
+    login = '';
+    while (login.length < 3) {
+        login += abc[Math.floor(Math.random() * abc.length)];
+    };
+     //console.log(login);
+} while (login != 'zaa');console.log("OK");
+for (s=4;)
